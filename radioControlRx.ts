fc39e370110f9e1ecd0447e449f322f9d6bf6a3c
KierPalin <45743174+KierPalin@users.noreply.github.com>
2024-12-22 02:38:17 +0000

    Fixed MicroData draw data being off-screen issue

--- radioControlRx.ts
+++ radioControlRx.ts
@@ -19,7 +19,11 @@ const SCREEN_FN_ID_SET_PIXEL_XFRM: number = 22;
 const SCREEN_FN_ID_PRINT: number = 23;
 
 import Screen = user_interface_base.Screen
-
+// 15, 17, 18, 160, 128, 12
+// 23, 253, 248, 11,
+// 23, 44, 57, 11, 
+// 9, 182, 43, 213, 43, 9
+// 15, 176, 192, 160
 function radioControlRxLoop() {
     let latestString: string = "";
 
@@ -33,6 +37,11 @@ function radioControlRxLoop() {
         const fn_id: number = buffer[0];
         const params: Buffer = buffer.slice(1);
 
+        // basic.showString("S")
+        // basic.showNumber(fn_id)
+        // for (let i = 0; i < params.length; i++) {
+        //     basic.showString("" + i + " : " + params[i])
+        // }
 
         // basic.showString("Y")
         // basic.showNumber(fn_id);
@@ -45,19 +54,20 @@ function radioControlRxLoop() {
             case SCREEN_FN_ID_RESET_SCREEN_IMAGE: { Screen.resetScreenImage(); break; }
             case SCREEN_FN_ID_SET_IMAGE_SIZE: { Screen.setImageSize(params[0], params[1]); break; }
 
+            // semi-cheat; the assets are pre-loaded.
             case SCREEN_FN_ID_DRAW_TRANSPARENT_IMAGE: {
                 const from: Bitmap = microdata.icons.get(latestString);
-                Screen.drawTransparentImage(from, params[1], params[2]);
+                Screen.drawTransparentImage(from, params[1] - Screen.HALF_WIDTH, params[2] - Screen.HALF_HEIGHT);
                 break;
             }
 
-            case SCREEN_FN_ID_DRAW_LINE: { Screen.drawLine(params[0], params[1], params[2], params[3], params[4]); break; }
-            case SCREEN_FN_ID_DRAW_RECT: { Screen.drawRect(params[0], params[1], params[2], params[3], params[4]); break; }
+            case SCREEN_FN_ID_DRAW_LINE: { Screen.drawLine(params[0] - Screen.HALF_WIDTH, params[1] - Screen.HALF_HEIGHT, params[2] - Screen.HALF_WIDTH, params[3] - Screen.HALF_HEIGHT, params[4]); break; }
+            case SCREEN_FN_ID_DRAW_RECT: { Screen.drawRect(params[0] - Screen.HALF_WIDTH, params[1] - Screen.HALF_HEIGHT, params[2], params[3], params[4]); break; }
             case SCREEN_FN_ID_FILL: { Screen.fill(params[0]); break; }
-            case SCREEN_FN_ID_FILL_RECT: { Screen.fillRect(params[0], params[1], params[2], params[3], params[4]); break; }
-            case SCREEN_FN_ID_SET_PIXEL: { Screen.setPixel(params[0], params[1], params[2]); break; }
+            case SCREEN_FN_ID_FILL_RECT: { Screen.fillRect(params[0] - Screen.HALF_WIDTH, params[1] - Screen.HALF_HEIGHT, params[2], params[3], params[4]); break; }
+            case SCREEN_FN_ID_SET_PIXEL: { Screen.setPixel(params[0] - Screen.HALF_WIDTH, params[1] - Screen.HALF_HEIGHT, params[2]); break; }
 
-            case SCREEN_FN_ID_PRINT: { Screen.print(latestString, params[1], params[2], params[3]); break; }
+            case SCREEN_FN_ID_PRINT: { Screen.print(latestString, params[0] - Screen.HALF_WIDTH, params[1] - Screen.HALF_HEIGHT, params[2]); break; }
 
             default: { break; }
         }
