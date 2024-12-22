c29524deeb6d0b6c6f21f72fff9b19ac72072818
KierPalin <45743174+KierPalin@users.noreply.github.com>
2024-12-22 15:09:03 +0000

    radioControlRx.ts

--- radioControlRx.ts
+++ radioControlRx.ts
@@ -1,56 +1,35 @@
 const SCREEN_FN_ID_RESET_SCREEN_IMAGE: number = 5;
 const SCREEN_FN_ID_SET_IMAGE_SIZE: number = 6;
 const SCREEN_FN_ID_DRAW_TRANSPARENT_IMAGE: number = 7;
-const SCREEN_FN_ID_DRAW_TRANSPARENT_IMAGE_XFRM: number = 8;
 const SCREEN_FN_ID_DRAW_LINE: number = 9;
-const SCREEN_FN_ID_DRAW_LINE_XFRM: number = 10;
-const SCREEN_FN_ID_DRAW_LINE_SHADED: number = 11;
 const SCREEN_FN_ID_DRAW_RECT: number = 12;
-const SCREEN_FN_ID_DRAW_RECT_XFRM: number = 13;
 const SCREEN_FN_ID_FILL: number = 14;
 const SCREEN_FN_ID_FILL_RECT: number = 15;
-const SCREEN_FN_ID_FILL_RECT_XFRM: number = 16;
-const SCREEN_FN_ID_FILL_BOUNDS_XFRM: number = 17;
-const SCREEN_FN_ID_DRAW_BOUNDS_XFRM: number = 18;
-const SCREEN_FN_ID_OUTLINE_BOUNDS_XFRM: number = 19;
-const SCREEN_FN_ID_OUTLINE_BOUNDS_XFRM4: number = 20;
 const SCREEN_FN_ID_SET_PIXEL: number = 21;
-const SCREEN_FN_ID_SET_PIXEL_XFRM: number = 22;
 const SCREEN_FN_ID_PRINT: number = 23;
 
 import Screen = user_interface_base.Screen
-// 15, 17, 18, 160, 128, 12
-// 23, 253, 248, 11,
-// 23, 44, 57, 11, 
-// 9, 182, 43, 213, 43, 9
-// 15, 176, 192, 160
 function radioControlRxLoop() {
     let latestString: string = "";
 
     radio.onReceivedString((recievedString: string) => {
         basic.showString("R");
         latestString = recievedString;
+        basic.showNumber(latestString.length);
     })
+
+    radio.setGroup(5)
+    radio.setFrequencyBand(14)
 
 
     radio.onReceivedBuffer((buffer: Buffer) => {
         const fn_id: number = buffer[0];
         const params: Buffer = buffer.slice(1);
 
-        // basic.showString("S")
-        // basic.showNumber(fn_id)
-        // for (let i = 0; i < params.length; i++) {
-        //     basic.showString("" + i + " : " + params[i])
-        // }
-
-        // basic.showString("Y")
-        // basic.showNumber(fn_id);
-        // basic.showString("N");
-        // basic.showNumber(params[0]);
-        // basic.showString("n");
-        // basic.clearScreen();
+        basic.showString("R")
 
         switch (fn_id) {
+            // case SCREEN_FN_ID_ASSET_SETUP: { break;}
             case SCREEN_FN_ID_RESET_SCREEN_IMAGE: { Screen.resetScreenImage(); break; }
             case SCREEN_FN_ID_SET_IMAGE_SIZE: { Screen.setImageSize(params[0], params[1]); break; }
 
@@ -71,5 +50,6 @@ function radioControlRxLoop() {
 
             default: { break; }
         }
+        basic.clearScreen()
     })
 }
